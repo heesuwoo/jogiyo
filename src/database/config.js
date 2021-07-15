52065770aedd3858427a23c8d144c6219930a50f
wooheesu <hsoo321@naver.com>
2021-07-15 16:35:51 +0900

    DB 연결 정보 수정

--- src/database/config.js
+++ src/database/config.js
@@ -6,9 +6,21 @@ module.exports = mysql.createPool({
   // password: process.env.DB_PW,
   // database: process.env.DB_DNAME,
   // port: process.env.DB_PORT,
-  user: "root",
-  host: "localhost",
-  password: "1234",
-  database: "project",
-  port: "3306",
+
+  // user: "root",
+  // host: "localhost",
+  // password: "1234",
+  // database: "project",
+  // port: "3306",
+
+  user: "b90ecf2d9ecce0",
+  host: "us-cdbr-east-04.cleardb.com",
+  password: "42a8f899",
+  database: "heroku_d4642c56bf8158e",
 });
+
+// user: b90ecf2d9ecce0
+// host: us-cdbr-east-04.cleardb.com
+// passwordDB :heroku_d4642c56bf8158e
+// pw :42a8f899
+// db: heroku_d4642c56bf8158e
