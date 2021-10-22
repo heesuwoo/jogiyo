a9d657f3d0f247d48aa7b30ea1b1914a4d060763
heesu <hsoo321@naver.com>
2021-10-22 21:34:53 +0900

    DB 연결 정보 수정

--- src/database/config.js
+++ src/database/config.js
@@ -8,24 +8,24 @@ module.exports = mysql.createPool({
   // port: process.env.DB_PORT,
 
   // mysql db
-  // user: "root",
-  // host: "localhost",
-  // password: "1234",
-  // database: "project",
-  // port: "3306",
+  user: "root",
+  host: "localhost",
+  password: "1234",
+  database: "project",
+  port: "3306",
 
   //heroku db
   //mysql -u b90ecf2d9ecce0 -h us-cdbr-east-04.cleardb.com -p heroku_d4642c56bf8158e
   //비밀번호: 42a8f899
 
-  user: "b90ecf2d9ecce0",
-  host: "us-cdbr-east-04.cleardb.com",
-  password: "42a8f899",
-  database: "heroku_d4642c56bf8158e",
+  // user: "b90ecf2d9ecce0",
+  // host: "us-cdbr-east-04.cleardb.com",
+  // password: "42a8f899",
+  // database: "heroku_d4642c56bf8158e",
 });
 
-// user: b90ecf2d9ecce0
-// host: us-cdbr-east-04.cleardb.com
+// user: b90ecf2d9ecce0 - user
+// host: us-cdbr-east-04.cleardb.com - ip
 // passwordDB :heroku_d4642c56bf8158e
 // pw :42a8f899
 // db: heroku_d4642c56bf8158e
