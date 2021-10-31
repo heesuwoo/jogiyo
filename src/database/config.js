a00eb1ae514fbf33bd03ceb1354d57b60c12591f
heesu <hsoo321@naver.com>
2021-10-31 16:51:47 +0900

    DB 연결 정보 수정

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,5 @@
 // const mysql = require("mysql2/promise");
-const mysql = require("mysql");
+const mysql = require("mysql/promise");
 
 module.exports = mysql.createPool({
   // user: process.env.DB_UNAME,
